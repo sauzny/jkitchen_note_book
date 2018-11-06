02646c7ca6eca5dc0537e72936f5f32f7eda4a3e
xinxin <xinxin_949421@126.com>
2018-11-06 10:39:06 +0800

    by: xinxin httpclient :disappointed:

---
+++ jdk11examples/src/main/java/com/sauzny/httpclient/HttpClientEx.java
@@ -0,0 +1,82 @@
+package com.sauzny.httpclient;
+
+import org.springframework.util.StopWatch;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+
+public class HttpClientEx {
+
+    // 我测试了一个get觉得性能不好，其余的我就不测试了
+    // http http2 websocket 都有client
+
+    public static void get() throws IOException, InterruptedException {
+
+        StopWatch task = new StopWatch("jdk11 http get demo");
+
+        var uri = "https://api.github.com/users/sauzny/repos";
+
+        task.start();
+
+        // 这个变量实际情况下应该是每次都是new的 因为参数不同
+        // 这个变量的创建不是很耗时 0-3ms，所以我没有把这个全局初始化
+        var request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+
+        task.stop();
+
+        task.start();
+
+        // 已经全局初始化过了，很快
+        var client = HttpClient.newHttpClient();
+
+        task.stop();
+
+        task.start();
+
+        // 本机postman  记录 805 566 517 546
+        // 本机浏览器    记录 799 519 485 504 547
+
+        // 这个执行比较慢 记录 2410 1159 1469 2146 1617
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        task.stop();
+
+        System.out.println(task.prettyPrint());
+
+        System.out.println(response.statusCode());
+    }
+
+    public static void post(){
+        HttpRequest.newBuilder()
+                .uri(URI.create(""))
+                .POST(HttpRequest.BodyPublishers.ofString("", StandardCharsets.UTF_8))
+                .build();
+        // 这个当然也可以上传文件或者Multipart
+    }
+
+    public static void main(String[] args) {
+
+        // 初始化这个client需要的时间比较长 500ms左右，应该有些东西是JVM全局初始
+        // 在这执行之后，后面的get里就会是0-1ms
+        var client = HttpClient.newHttpClient();
+
+        try {
+            for(int i=0; i<5; i++){
+                // 测试的结果看 很不好
+                HttpClientEx.get();
+                Thread.sleep(3*1000L);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
