3fd330f3af1a25504da6119f506e0d8a873cf3e6
sauzny <xinxin_949421@yahoo.com>
2020-05-08 16:11:02 +0800

    add demo

--- jkitchen_note/src/main/java/com/sauzny/jkitchen_note/stream/Paixu.java
+++ jkitchen_note/src/main/java/com/sauzny/jkitchen_note/stream/Paixu.java
@@ -2,6 +2,7 @@ package com.sauzny.jkitchen_note.stream;
 
 import com.google.common.collect.Lists;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Paixu {
@@ -15,7 +16,7 @@ public class Paixu {
         StudentPaixu s5 = new StudentPaixu(9, "ee");
         
         List<StudentPaixu> list = Lists.newArrayList(s1, s2, s3, s4, s5);
-        
+
         System.out.println(list);
         
         // 按照id升序排列
@@ -37,6 +38,27 @@ public class Paixu {
         list.sort((StudentPaixu p1, StudentPaixu p2) -> p2.getName().compareTo(p1.getName()) );
         
         System.out.println(list);
+
+        for(int j=100; j<1000; j++){
+            list.add(new StudentPaixu(j, ""));
+        }
+
+        // 找出id最小的
+        StudentPaixu minId = list.parallelStream().min(Comparator.comparingInt(StudentPaixu::getId)).get();
+        System.out.println(minId);
+
+        // 找出id最小的
+        StudentPaixu maxId = list.parallelStream().max(Comparator.comparingInt(StudentPaixu::getId)).get();
+        System.out.println(maxId);
+
+
+        // 丢数据测试！
+        List<Integer> tlist = Lists.newArrayList();
+
+        list.parallelStream().forEach(s -> tlist.add(s.getId()));
+
+        System.out.println(tlist);
+
     }
 }
 
@@ -70,5 +92,12 @@ class StudentPaixu {
     public StudentPaixu() {
         super();
     }
-   
+
+    @Override
+    public String toString() {
+        return "StudentPaixu{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
